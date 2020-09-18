6ad2d9d5d6298a9e78065c8432837d4d5c06fda9
Nilzao <288571+nilzao@users.noreply.github.com>
2020-09-17 23:15:09 -0400

    sb-persona and sb-openfire-api fixes

--- src/main/java/io/github/sbcloudrace/sbpersona/persona/SbPersonaController.java
+++ src/main/java/io/github/sbcloudrace/sbpersona/persona/SbPersonaController.java
@@ -6,6 +6,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @Controller
@@ -19,5 +20,14 @@ public class SbPersonaController {
     @ResponseBody
     public List<SbPersona> findByUserId(@PathVariable Long userId) {
         return sbPersonaRepository.findByUserId(userId);
+    }
+
+    @RequestMapping("/personasIdsByUser/{userId}")
+    @ResponseBody
+    public List<Long> findIdsByUserId(@PathVariable Long userId) {
+        List<Long> personaIds = new ArrayList<>();
+        sbPersonaRepository.findByUserId(userId)
+                .forEach(sbPersona -> personaIds.add(sbPersona.getId()));
+        return personaIds;
     }
 }
